1705fcd933e9652936ec9d0a990131a7a0368c79
yolf <gleoros@gmail.com>
2022-01-31 12:23:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let order=[];
+  let count={};
+
+  for(bracket of str){
+    let index = bracketsConfig.findIndex( v => v[0]==bracket);
+    if(index==-1 || count[index]>0 && bracketsConfig[index][0]==bracketsConfig[index][1]){
+      let closeIndex = bracketsConfig.findIndex( v => v[1]==bracket);
+      if(closeIndex==order[order.length-1]){
+        count[closeIndex]--;
+        order.pop();
+      }else {
+        return false
+      }
+
+    }else{
+      order.push(index);
+      count[index] = count[index]!=undefined ? count[index]+1 : 1;
+
+    }
+  }
+
+  return order.length==0;
 }
